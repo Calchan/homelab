6312ba92948b409fe274acedbe2abf4640da1d85
Denis Dupeyron <denis.dupeyron@gmail.com>
2023-07-30 11:50:45 -0600

    Add terraform/misc/update

---
+++ terraform/misc/update/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+    }
+  }
+}
+
+module "hosts" {
+  source = "../../modules/hosts"
+}
+
+resource "null_resource" "update" {
+  for_each = "${module.hosts.all_ips}"
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+  connection {
+    host = each.value
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "export DEBIAN_FRONTEND=noninteractive",
+      "apt-get -q update",
+      "apt-get dist-upgrade -q -m -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold'",
+      "apt-get -q -y autopurge",
+      "shutdown -r now",
+    ]
+  }
+}
